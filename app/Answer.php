490766511eddc3a1146dd529f88c7f4635c3aa18
Al Mahafzah <aam34@njit.edu>
2018-07-22 20:36:01 -0400

    Added Answer, model and table answer seeder and added tests for user profile and answer test.

--- app/Answer.php
+++ app/Answer.php
@@ -4,11 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
+class Answer extends Model
 {
+
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function question()
+    {
+        return $this->belongsTo('App\Question');
     }
     public function answers()
     {
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -15,16 +15,27 @@ class UserTest extends TestCase
      */
     public function testSave()
 
-        {
-            $user = factory(\App\User::class)->make();
+    {
+        $user = factory ( \App\User::class )->make ();
 
-            $this->assertTrue($user->save());
-        }
+        $this->assertTrue ( $user->save () );
+    }
 
-        public function testQuestions()
-        {
-        $user = factory(\App\User::class)->make();
-        $this->assertTrue(is_object($user->questions()->get()));
-         }
+    public function testQuestions()
+    {
+        $user = factory ( \App\User::class )->make ();
+        $this->assertTrue ( is_object ( $user->questions ()->get () ) );
+    }
 
+    public function testAnswers()
+    {
+        $user = factory ( \App\User::class )->make ();
+        $this->assertTrue ( is_object ( $user->answers ()->get () ) );
+    }
+
+    public function testProfile()
+    {
+        $user = factory ( \App\User::class )->make ();
+        $this->assertTrue ( is_object ( $user->profile ()->get () ) );
+    }
 }
