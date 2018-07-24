66429f7be7354b4f51db42afe8fed3d0c0bd20b7
Al Mahafzah <aam34@njit.edu>
2018-07-24 14:13:55 -0400

    Added question, answer and profile views. Made updates to the app.blade.php view and updated routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+
+Route::get('/questions/{question_id}/answers/create', 'AnswerController@create')->name('answer.create');
+
+Route::resources([
+    'question' => 'QuestionController',
+]);
