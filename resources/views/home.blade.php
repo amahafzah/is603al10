1c8b760e80180baad15943940738a1416bc5455c
Al Mahafzah <aam34@njit.edu>
2018-07-26 08:21:09 -0400

    Updated home.blade to ask multiple questions.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Questions
-                        <a class="btn btn-primary float-right" href="#">
+                        <a class="btn btn-primary float-right" href="{{route ('questions.create')}}">
                             Create a Question
                         </a>
 
@@ -37,7 +37,7 @@
                                         </div>
                                     </div>
                                 @empty
-                                    <a class="btn btn-primary" href="{{route ('questions.create')}}">
+
                                     There are no questions to view, you can click to create a question.
                                     </a>
                                     @endforelse
